d9fe446fd4571239461d4dbba9d96f2d6b4c2373
proarabic <30710501+proarabic@users.noreply.github.com>
2018-07-26 16:58:06 +0100

    Delete loader.js

--- loader.js
+++
@@ -1,1 +0,0 @@
-$(document)['ready'](function(){$('#tha')['html']('<a href='https://www.tharabic.com' id='tha' rel='dofollow' target='_blank'> المحترف العربي </a>'),setInterval(function(){$('#dontedit:visible')['length']||(window['location']['href']='https://www.tharabic.com')},3e3)});
